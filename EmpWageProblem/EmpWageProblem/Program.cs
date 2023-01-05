7238425eff1b773e45d3ac8ae64c2130a442d9cd
Sankalp <sankalp.kulkarni05@gmail.com>
2023-01-05 12:07:11 +0530

    [Sankalp]-welcome mwssage

---
+++ EmpWageProblem/EmpWageProblem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmpWageProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to emp Management!");
+        }
+    }
+}
